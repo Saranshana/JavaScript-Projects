c0bd8ddca029ea1ecf914cdcd0cc37ba110c8d1a
Saranshana <saranyas1986@yahoo.com>
2026-09-24 14:09:33 -0400

    Expression Assignment

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,51 @@
+
+//Document.write method assignemnt
+document.write("Hello World!"); 
+
+//Variable Assignemnt
+var A="This is a string"; 
+document.write(A+"<br>"); 
+
+//String Assignment
+var B="Its Javascript"; 
+window.alert(B);
+
+//Escape Character Assignemnt
+var C="She said,\"Hello!\"";
+document.write(C+"<br>");
+
+//Concatenation Assignemnt
+document.write("\"By small step, so don\'t lose heart,"+"And an ocean filled drop by drop.\""+"<br>")
+  
+//Multiple variable Assignemnt
+var sentence1 = "By small step, so don't lose heart.",
+    sentence2 = "And an ocean filled drop by drop.",
+    sentence3 = "Into that space where everything is silent, empty.";
+
+document.write(sentence3+"<br>");
+
+//Expression assignment
+var result = 10 + 20;
+document.write(result+"<br>");
+
+//Creating the first variable 
+var sent1=("This is the begining of the string");
+
+//Creating second variable
+var sent2=("and this is the end of the string");
+
+//concatenate sent1 and sent2
+var fullsentence= sent1+sent2;
+
+//Display the concatenated string in an alert window
+window.alert(fullsentence);
+
+document.write(fullsentence);
+
+// The expression adds two numbers together.
+var number1 = 10;
+var number2 = 5;
+var result = number1 + number2;
+
+// Display the result of the expression.
+window.alert("The result of the expression is: " + result);
